0137b83a71e3496aff2a6783317d1949ce7a9049
Bashar <bamin@avertra.com>
2025-05-15 13:33:48 +0300

    add more crud features

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -11,8 +11,10 @@ import {
   useQuery,
 } from "@tanstack/react-query";
 import AddProjectForm from "./AddProjectForm";
+import { useRef } from "react";
 
 function Hello() {
+  const abortController = useRef(new AbortController());
   const navigate = useNavigate();
   const { data, isLoading, refetch } = useQuery({
     queryKey: ["projects"],
@@ -23,37 +25,21 @@ function Hello() {
     },
   });
 
-  const { mutate, isPending } = useMutation({
-    mutationFn: (meta) =>
-      fetch("http://localhost:3000/api/create-build", {
+  const { mutate, isPending, variables } = useMutation({
+    mutationFn: async ({ meta, abortController }) => {
+      console.log(meta, abortController);
+      return fetch("http://localhost:3000/api/create-build", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(meta),
+        signal: abortController.current.signal,
       })
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
-        }),
+        });
+    },
   });
-
-  // const [output, setOutput] = useState("");
-
-  // useEffect(() => {
-  //   const eventSource = new EventSource("http://localhost:3001/stream");
-
-  //   eventSource.onmessage = (event) => {
-  //     setOutput((prev) => prev + event.data + "\n");
-  //   };
-
-  //   eventSource.onerror = (err) => {
-  //     console.error("SSE error", err);
-  //     eventSource.close();
-  //   };
-
-  //   return () => {
-  //     eventSource.close();
-  //   };
-  // }, []);
 
   const { data: javaMeta } = useQuery({
     queryKey: ["java_meta"],
@@ -63,24 +49,85 @@ function Hello() {
     },
   });
 
+  const { mutate: deleteProject } = useMutation({
+    mutationFn: async ({ project }) => {
+      console.log(project);
+      return fetch("http://localhost:3000/api/project", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ project }),
+      });
+    },
+    onSuccess: () => refetch(),
+  });
+
   if (isLoading) return <div>loading ...</div>;
 
   return (
     <div>
+      <div className="cta-contaienr">
+        <h4>Create Mendix Native App bundles</h4>
+        <button
+          onClick={() => navigate("/add-project")}
+          className="action-button"
+        >
+          add project
+        </button>
+      </div>
       {data.map((meta, index) => (
         <div className="project" key={index}>
           {meta.project.split("\\").at(-1)}
-          <button
-            className="build-btn"
-            onClick={() => {
-              mutate(meta);
-            }}
-          >
-            {isPending ? <span class="loader"></span> : "Build"}
-          </button>
+          <div className="action-container">
+            <button
+              className={`build-btn${isPending ? " build-disabled" : ""} me-1`}
+              onClick={() => {
+                if (isPending) return;
+                mutate({ meta, abortController });
+              }}
+            >
+              {isPending && variables.meta.project === meta.project ? (
+                <span class="loader"></span>
+              ) : (
+                "Build"
+              )}
+            </button>
+
+            <button
+              className={`action-button ${
+                isPending && variables.meta.project === meta.project
+                  ? "shown me-1"
+                  : "hidden"
+              }`}
+              onClick={() => {
+                void fetch("http://localhost:3000/api/stop-build", {
+                  method: "POST",
+                });
+              }}
+            >
+              Stop
+            </button>
+
+            <button
+              className="action-button me-1"
+              onClick={() =>
+                navigate("/add-project", {
+                  state: meta,
+                })
+              }
+            >
+              Edit
+            </button>
+            <button
+              className="remove-button"
+              onClick={() => {
+                deleteProject({ project: meta.project });
+              }}
+            >
+              Remove
+            </button>
+          </div>
         </div>
       ))}
-      <button onClick={() => navigate("/add-project")}>add project</button>
     </div>
   );
 }
